a8b38d288cba2931cb52ce102c73e7d86e853220
Pedro Henrique <pedrohenr1qqstudios@gmail.com>
2024-10-14 19:44:40 -0300

    Fix: Correcting route namees in User router

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -8,20 +8,20 @@ const authMiddleware = require('../middleware/auth.middleware');
 // Create routes
 // GET
 router.get('/findById/:id', authMiddleware, userController.findUserById);
-router.get('/findAll' ,userController.findAllUsers);
+router.get('/findAll', authMiddleware, userController.findAllUsers);
 
 // POST
 router.post('/create', userController.createUser);
-router.post('/createAddress/:id', userController.addAddress);
-router.post('/createFavProduct', userController.addFavProduct);
+router.post('/addAddress/:id', authMiddleware,  userController.addAddress);
+router.post('/addFavProduct/:id', authMiddleware, userController.addFavProduct);
 
 // PUT
-router.put('/update/:id', userController.updateUser);
+router.put('/update/:id', authMiddleware, userController.updateUser);
 
 // DELETE
-router.delete('/delete/:id', userController.deleteUser);
-router.delete('/deleteAddress/:id', userController.removeAddress);
-router.delete('/deleteFavProduct/:id', userController.removeFavProduct);
+router.delete('/delete/:id', authMiddleware, userController.deleteUser);
+router.delete('/removeAddress/:id', authMiddleware, userController.removeAddress);
+router.delete('/removeFavProduct/:id', authMiddleware, userController.removeFavProduct);
 
 
 module.exports = router;
