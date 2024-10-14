e4be376bf121d44ca5a5d8f819e171ef38563e40
Pedro Henrique <pedrohenr1qqstudios@gmail.com>
2024-10-14 12:37:58 -0300

    Fix: changing env variable name (URLDATABASE --> URL_DATABASE)

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 // Connect to database
 function connectToDatabase(){
-  mongoose.connect(process.env.URLDATABASE)
+  mongoose.connect(process.env.URL_DATABASE)
   .then(() => console.log("DATABASE connected"))
   .catch((err) => console.log("Error in DATABASE connection: "+err));
 }
