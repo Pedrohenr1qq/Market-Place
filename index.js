5d17f99cc00d7e29f31b1aacf8480ae076963221
Pedro Henrique <pedrohenr1qqstudios@gmail.com>
2024-10-15 10:43:04 -0300

    Feat: Add CRUD Order

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const authRouter = require('./src/router/auth.router');
 const productRouter = require('./src/router/product.router');
 const categoryRouter = require('./src/router/category.router');
 const shoppCartRouter = require('./src/router/shoppCart.router');
+const orderRouter = require('./src/router/order.router');
 
 // Starting database connection
 connectToDatabase();
@@ -24,6 +25,7 @@ app.use('/user', userRouter);
 app.use('/product', productRouter);
 app.use('/category', categoryRouter);
 app.use('/shoppCart', shoppCartRouter);
+app.use('/order', orderRouter);
 
 // Main route
 app.get('/', (req,res) => {
